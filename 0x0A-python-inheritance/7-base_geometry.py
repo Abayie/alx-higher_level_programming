45280772a8839eb70a07ae2ff48a158c0594d1f1
Abayie <jonathanboahene@gmail.com>
2022-07-06 05:57:04 +0000

    [Update]: Fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""BaseGeometry class module"""
+"""
+Module 7-base_geometry
+
+Contains BaseGeometry
+with public instance method area and integer_validation
+"""
 
 
 class BaseGeometry:
-    """Geomerty utility class"""
-
+    """
+    Methods:
+        area(self)
+        integer_validator(self, name, value)
+    """
     def area(self):
-        """Returns the area"""
+        """not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value"""
-
+        """validates input
+        Args:
+            name (str): assumed always a string
+            value (int): greater than 0
+        """
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
