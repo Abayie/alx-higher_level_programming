3fb873df5e3477c25f93e38bcabd88af362969dd
Abayie <jonathanboahene@gmail.com>
2022-07-07 02:13:33 +0000

    [Update]: Fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""
-Module 1-my_list
-
-Contains class MyList
-inherits from list; has public instance method to print sorted
-"""
+"""The ``MyList`` module"""
 
 
 class MyList(list):
-    """inherits from list
+    """MyList inherting from the ``list`` class"""
 
-    methods:
-    print_sorted(self)
-    """
     def print_sorted(self):
-        """prints list of ints all sorted in ascending order"""
+        """Print the list as sorted"""
         print(sorted(self))
